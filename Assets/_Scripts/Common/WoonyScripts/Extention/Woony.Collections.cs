b247252118d632c8d791561791f2f01a48eab8be
Woony <82883758+kksumm123@users.noreply.github.com>
2024-11-19 16:20:46 +0900

    [리팩토링] Woony.Collections 신규 메소드 추가 및 리팩토링.

--- Assets/_Scripts/Common/WoonyScripts/Extention/Woony.Collections.cs
+++ Assets/_Scripts/Common/WoonyScripts/Extention/Woony.Collections.cs
@@ -6,7 +6,14 @@ using Random = UnityEngine.Random;
 
 public static partial class Woony
 {
-    public static T GetRandomItem<T>(this List<T> list)
+    public static T SafeGetItem<T>(this IList<T> list, int index)
+    {
+        return list == null || list.Count < index
+            ? default
+            : list[index];
+    }
+
+    public static T GetRandomItem<T>(this IList<T> list)
     {
         if (list.Count == 0)
         {
@@ -17,7 +24,7 @@ public static partial class Woony
         return list[Random.Range(0, list.Count)];
     }
 
-    public static T GetLastItem<T>(this List<T> list)
+    public static T GetLastItem<T>(this IList<T> list)
     {
         if (list.Count == 0)
         {
@@ -28,46 +35,12 @@ public static partial class Woony
         return list[list.Count - 1];
     }
 
-    public static T GetLastItem<T>(this T[] array)
-    {
-        if (array.Length == 0)
-        {
-            Debug.LogError("비어있는 list의 값을 얻으려고 듬.");
-            return default(T);
-        }
-
-        return array[array.Length - 1];
-    }
-
-    public static T[] Shuffle<T>(this T[] array)
-    {
-        return Shuffle(array, array.Length);
-    }
-
-    public static T[] Shuffle<T>(this T[] array, int range)
-    {
-        int n = range;
-        int m = 0;
-        T tempItem;
-
-        while (n > 1)
-        {
-            n--;
-            m = Random.Range(0, n + 1);
-            tempItem = array[m];
-            array[m] = array[n];
-            array[n] = tempItem;
-        }
-
-        return array;
-    }
-
-    public static List<T> Shuffle<T>(this List<T> list)
+    public static IList<T> Shuffle<T>(this IList<T> list)
     {
         return Shuffle(list, list.Count);
     }
 
-    public static List<T> Shuffle<T>(this List<T> list, int range)
+    public static IList<T> Shuffle<T>(this IList<T> list, int range)
     {
         int n = range;
         int m = 0;
@@ -85,7 +58,7 @@ public static partial class Woony
         return list;
     }
 
-    public static List<T> RemoveAtUnordered<T>(this List<T> list, int index)
+    public static IList<T> RemoveAtUnordered<T>(this IList<T> list, int index)
     {
         if (list.Count == 0)
         {
@@ -111,13 +84,17 @@ public static partial class Woony
         return dic;
     }
 
-    public static T GetRandomItemByProb<T>(this List<T> list1, List<int> probs)
+    public static T GetRandomItemByProb<T>(this IList<T> list1, IList<int> probs)
     {
         int randomProb, totalProb = 0;
 
-        probs.ForEach(x => totalProb += x);
-        randomProb = Random.Range(0, totalProb);
         int count = probs.Count;
+        for (int i = 0; i < count; i++)
+        {
+            totalProb += probs[i];
+        }
+
+        randomProb = Random.Range(0, totalProb);
         for (int i = 0; i < count; i++)
         {
             if (probs[i] < randomProb)
